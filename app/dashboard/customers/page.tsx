3d7242006c14019e83906b689f5d3f4f80beaabf
Ramp_ <105841115123@student.unismuh.ac.id>
2025-08-13 00:49:37 +0800

    update customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+// PERBAIKAN: Menyesuaikan tipe props untuk Next.js 15+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }

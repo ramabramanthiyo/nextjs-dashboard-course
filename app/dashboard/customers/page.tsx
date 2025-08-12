24457e37f1e778100e395950bc7e73f1fbe4f4fa
Ramp_ <105841115123@student.unismuh.ac.id>
2025-08-13 00:59:30 +0800

    update customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(props: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  // Tunggu sampai searchParams ter-resolve
+  const { query } = await props.searchParams;
+
+  // Ambil data customers sesuai query (kalau undefined, pakai string kosong)
+  const customers = await fetchFilteredCustomers(query ?? '');
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
